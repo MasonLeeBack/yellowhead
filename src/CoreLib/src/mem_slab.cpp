0698e21a1c07e07439714a495cbdf1c038c14d15
Mason Back <masonleeback@gmail.com>
2026-06-05 16:38:04 -0400

    Recover reserved memory dirty allocation

--- src/CoreLib/src/mem_slab.cpp
+++ src/CoreLib/src/mem_slab.cpp
@@ -1,5 +1,7 @@
 #include "Mem.h"
 
+#include <stdlib.h>
+
 static u32 MakeAligned(u32 size, u32 align)
 {
     return (size + align - 1) & ~(align - 1);
@@ -12,12 +14,12 @@ static u32 Align(u32 size, u32 align)
 
 void* CReservedMemory::DirtyAlloc(u32 size)
 {
-    Size = size;
+    if (Size < size)
+        _Exit(1);
+
     return Data;
 }
 
 void CReservedMemory::DirtyFree()
 {
-    Data = 0;
-    Size = 0;
 }
