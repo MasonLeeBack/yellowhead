592144ec869a56bb2e8de81b44f6f85dff2d1abe
Mason Back <masonleeback@gmail.com>
2026-06-05 16:15:47 -0400

    Recover PPU thread sleep wrappers

--- src/CoreLib/src/cell/thread.cpp
+++ src/CoreLib/src/cell/thread.cpp
@@ -2,6 +2,7 @@
 #include "thread.h"
 
 #include <sys/ppu_thread.h>
+#include <sys/timer.h>
 
 bool GThreadInit;
 sys_ppu_thread_t GGameHostThreadID;
@@ -21,6 +22,56 @@ THREAD GetCurrentPPUThread()
 THREADID GetCurrentPPUThreadId()
 {
     return GetCurrentPPUThread();
+}
+
+THREAD CreatePPUThread(THREADPROC threadproc, u64 thread_arg, const char* name, int priority, int stacksize, bool joinable)
+{
+    THREAD rv = 0;
+    int ret = sys_ppu_thread_create(&rv, threadproc, thread_arg, priority, stacksize, joinable, name);
+    if (ret != 0)
+        return 0;
+    return rv;
+}
+
+void ExitPPUThread(u64 retval)
+{
+    sys_ppu_thread_exit(retval);
+}
+
+bool JoinPPUThread(THREAD thread, u64* retval)
+{
+    u64 local_retval;
+    u32 ret = false;
+
+    if (thread == 0)
+        return ret;
+    if (retval == 0)
+        retval = &local_retval;
+
+    ret = (u32)sys_ppu_thread_join(thread, retval) == 0;
+    return ret;
+}
+
+bool SetPPUThreadPriority(THREAD thread, int priority)
+{
+    int ret = sys_ppu_thread_set_priority(thread, priority);
+    return ret ? false : true;
+}
+
+void ThreadSleep(int ms)
+{
+    if (ms == 0)
+        sys_ppu_thread_yield();
+    else
+        sys_timer_usleep(ms * 1000);
+}
+
+void ThreadSleepUS(int us)
+{
+    if (us == 0)
+        sys_ppu_thread_yield();
+    else
+        sys_timer_usleep(us);
 }
 
 bool InitThreads()
--- src/CoreLib/src/thread.h
+++ src/CoreLib/src/thread.h
@@ -2,6 +2,7 @@
 
 typedef u64 THREAD;
 typedef u64 THREADID;
+typedef void (*THREADPROC)(u64);
 
 bool AmInMainThread(void);
 bool AmInMainThreadOrSystemInit(void);
@@ -11,3 +12,11 @@ void SetGameHostThreadId(void);
 void ClearGameHostThreadId(void);
 THREAD GetCurrentPPUThread(void);
 THREADID GetCurrentPPUThreadId(void);
+THREAD CreatePPUThread(THREADPROC threadproc, u64 thread_arg, const char* name, int priority, int stacksize, bool joinable);
+void ExitPPUThread(u64 retval);
+bool JoinPPUThread(THREAD thread, u64* retval);
+bool SetPPUThreadPriority(THREAD thread, int priority);
+void ThreadSleep(int ms);
+void ThreadSleepUS(int us);
+
+extern volatile bool GsnGetLoadRequest;
