83d46d99f40c0adbdfe86ccf426ccc90f36d5cb1
Mason Back <masonleeback@gmail.com>
2026-06-05 22:18:48 -0400

    Align TEA locals with DWARF

--- src/CoreLib/src/TEA.cpp
+++ src/CoreLib/src/TEA.cpp
@@ -10,18 +10,22 @@ void CorrectedBlockTEA(EEncodeOrDecode encode_or_decode, EForceEncodeSmallPacket
         return;
 
     u32 y = v[0];
+    u32 z;
+    u32 sum;
+    u32 e;
     const u32 DELTA = 0x9e3779b9;
+    long p;
+    long q;
     if (encode_or_decode == E_TEA_ENCODE) {
-        u32 z = v[n - 1];
-        u32 sum = 0;
-        long q = 6 + 52 / n;
+        z = v[n - 1];
+        sum = 0;
+        q = 6 + 52 / n;
         if (q == 0)
             return;
         long rounds_remaining = q - 1;
         for (;;) {
             sum += DELTA;
-            u32 e = (sum >> 2) & 3;
-            long p;
+            e = (sum >> 2) & 3;
             for (p = 0; p < n - 1; ++p) {
                 y = v[p + 1];
                 z = v[p] += (((z >> 5) ^ (y << 2)) + ((y >> 3) ^ (z << 4))) ^ ((sum ^ y) + (key[(p & 3) ^ e] ^ z));
@@ -33,12 +37,10 @@ void CorrectedBlockTEA(EEncodeOrDecode encode_or_decode, EForceEncodeSmallPacket
             --rounds_remaining;
         }
     } else {
-        u32 z;
-        long q = 6 + 52 / n;
-        u32 sum = q * DELTA;
+        q = 6 + 52 / n;
+        sum = q * DELTA;
         while (sum != 0) {
-            u32 e = (sum >> 2) & 3;
-            long p;
+            e = (sum >> 2) & 3;
             for (p = n - 1; p > 0; --p) {
                 z = v[p - 1];
                 y = v[p] -= (((z >> 5) ^ (y << 2)) + ((y >> 3) ^ (z << 4))) ^ ((sum ^ y) + (key[(p & 3) ^ e] ^ z));
